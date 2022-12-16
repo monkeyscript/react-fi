3610ca7fd762ddd5595d46e14fd3458e1ca98013
MonkeyScript <monkeyscript.dev@gmail.com>
2022-12-16 13:01:49 +0530

    Update entry path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/lib/index.js"),
+      entry: path.resolve(__dirname, "src/lib/index.jsx"),
       name: "React-FI",
       fileName: (format) => `react-fi.${format}.js`,
     },
